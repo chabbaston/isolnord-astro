220320dbd725db40887da087bf25d56624ec6eef
chabbanadir <moh.chabba@gmail.com>
2025-08-07 12:18:05 +0100

    mobile edited

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'isolnord-orange': '#F58220',
+        'isolnord-blue': '#2A528A',
+        'isolnord-cyan': '#06b6d4',
+      },
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
